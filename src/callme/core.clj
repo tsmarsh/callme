ea4c18ed5704221a180460b64186735dd7d8dae5
Tom Marsh <ts.marsh@gmail.com>
2014-09-12 10:01:16 -0400

    Parses chunks

--- src/callme/core.clj
+++ src/callme/core.clj
@@ -1,8 +1,17 @@
 (ns callme.core
-  (:require [clojure.string :as s]))
+  (:require [clojure.string :as s]
+            [clj-time.format :as f]))
 
-;;; This is an incorrect implementation, such as might be written by
-;;; someone who was used to a Lisp in which an empty list is equal to
-;;; nil.
+(def date-formater (f/formatter "yyyy/MM/dd"))
+
 (defn split-on-empty-line [string]
   (s/split string #"\n\s*\n"))
+
+(defn parse-chunk [chunk]
+  (let [[interval date name & others] (s/split chunk #"\n")]
+    {:interval (read-string interval)
+     :date (f/parse date-formater date)
+     :name name}))
+
+(defn chunk-it [string]
+  (map parse-chunk (split-on-empty-line string)))
--- test/callme/core_test.clj
+++ test/callme/core_test.clj
@@ -1,6 +1,7 @@
 (ns callme.core-test
   (:use midje.sweet)
-  (:use [callme.core]))
+  (:use [callme.core])
+  (:require [clj-time.core :as t]))
 
 (def today "2011/12/11")
 
@@ -17,7 +18,30 @@ Every Six-Months
 2008/7/14
 Really Overdue")
 
+(def test-chunk
+"30
+2011/11/01
+Test Chunk")
 
-(facts "about `parsing a file`"
-       (fact "it splits the file into chunks"
-             (count (split-on-empty-line test-file)) => 3))
+(fact "it splits the file into chunks"
+             (count (split-on-empty-line test-file)) => 3)
+
+(fact "it can extract the interval from a chunk"
+      (:interval (parse-chunk test-chunk)) => 30)
+
+(fact "it can extract the data from a chunk"
+      (:date (parse-chunk test-chunk)) => (t/date-time 2011 11 1))
+
+(fact "it can extract the name from a chunk"
+      (:name (parse-chunk test-chunk)) => "Test Chunk")
+
+(facts "about 'chunk-it'"
+  (let [chunks (chunk-it test-file)]
+    (fact "there should be 3 chunks"
+        (count chunks) => 3)
+    (fact "this first chunk should be recently contacted"
+        (first chunks) => {:name "Recently Contacted"
+                           :date (t/date-time 2011 11 1)
+                           :interval 30})))
+
+
